c5dfea5e0a337bd4ba1cf6edadd287678fc3abc5
Johnvict <Johnvict20@gmail.com>
2020-11-20 13:09:22 +0100

    Completed intent sendind and receiving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { WebIntent } from '@ionic-native/web-intent/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+	SplashScreen,
+	WebIntent,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,18 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IntentOptions, WebIntent } from '@ionic-native/web-intent/ngx';
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+	selector: 'app-folder',
+	templateUrl: './folder.page.html',
+	styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+	public folder: string;
+	intentReplyForm: FormGroup = new FormGroup({
+		phone: new FormControl('', [Validators.required, Validators.pattern('[0]{1}[7-9]{1}[0-1]{1}[0-9]{8}')]),
+		name: new FormControl('', [Validators.required]),
+		amount: new FormControl(12000, [Validators.required]),
+		paid: new FormControl(false, [Validators.required]),
+		eventReference: new FormControl('', [Validators.nullValidator])
+	});
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+	constructor(
+		private webIntent: WebIntent,
+	) { }
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-  }
+	ngOnInit() {
+		//
+		console.log('HEY onInit WORKS!');
+		this.registerReceiver();
+	}
+	registerReceiver() {
+		this.webIntent.registerBroadcastReceiver({
+			filterActions: [
+				'com.darryncampbell.cordova.plugin.broadcastIntent.ACTION'
+			]
+		}).subscribe(intent => {
+			if (intent.extras.exitApp) {
+				console.log('APP EXITING');
+				// tslint:disable-next-line:no-string-literal
+				navigator['app'].exitApp();
+			}
+		});
 
+		this.webIntent.getIntent()
+			.then(intent => {
+				this.intentReplyForm.setValue({
+					...this.intentReplyForm.value,
+					...intent.extras
+				});
+			})
+			.catch(error => {
+				console.log('SOME ERROR OCCURED');
+				console.log(error);
+			});
+	}
+
+	sendIntent() {
+		console.log(this.intentReplyForm.value);
+		const options: IntentOptions = {
+			action: 'com.darryncampbell.cordova.plugin.broadcastIntent.ACTION',
+			package: 'com.devpitch.intent.sender',
+			extras: { ...this.intentReplyForm.value }
+		};
+
+		this.webIntent.sendBroadcast(options);
+		this.webIntent.sendResult({
+			extras: { ...this.intentReplyForm.value }
+		});
+	}
 }
